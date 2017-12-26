8a9fcff8f57ce13461716a21fb773dd4523b3f1d
Robert Bisewski <contact@ibiscybernetics.com>
2017-12-26 10:04:02 -0600

    linting item functions

--- item.go
+++ item.go
@@ -9,50 +9,50 @@ package main
 
 import "fmt"
 
-// Structure to hold the attributes of an item.
+// Item ... Structure to hold the attributes of an item.
 type Item struct {
 
-    // Holds the name of the given item.
-    name string
+	// Holds the name of the given item.
+	name string
 
-    // Holds the type of the given item.
-    category string
+	// Holds the type of the given item.
+	category string
 
-    // Store the current (x,y) coord of the item.
-    Y int
-    X int
+	// Store the current (x,y) coord of the item.
+	Y int
+	X int
 
-    // Appearance of the item.
-    ch rune
+	// Appearance of the item.
+	ch rune
 
-    // Pointer to the level of where the item is.
-    area *Area
+	// Pointer to the level of where the item is.
+	area *Area
 
-    // Determines whether an item can be equipped by a creature.
-    can_equip bool
+	// Determines whether an item can be equipped by a creature.
+	canEquip bool
 
-    // Variable that holds whether or not an item is broken.
-    is_broken bool
+	// Variable that holds whether or not an item is broken.
+	isBroken bool
 
-    // The current condition of an item. If the max durability and the
-    // current are equal, then the item is in perfect condition. 
-    durability_current int
-    durability_maximum int
+	// The current condition of an item. If the max durability and the
+	// current are equal, then the item is in perfect condition.
+	durabilityCurrent int
+	durabilityMaximum int
 
-    // Price of the item in gold coins.
-    price_to_purchase int
-    price_to_sell int
+	// Price of the item in gold coins.
+	priceToPurchase int
+	priceToSell     int
 
-    // Weight of the item in grams.
-    weight int
+	// Weight of the item in grams.
+	weight int
 
-    // Variables that determine how much an item increases (if positive)
-    // or decreases (if negative) the attack / defence of a creature. 
-    attack_increase int
-    defence_increase int
+	// Variables that determine how much an item increases (if positive)
+	// or decreases (if negative) the attack / defence of a creature.
+	attackIncrease  int
+	defenceIncrease int
 }
 
-//! Item constructor function.
+// NewItem ... Item constructor function.
 /*
  * @param     string    item name
  * @param     string    category
@@ -74,37 +74,37 @@ type Item struct {
  * @return    Item*    pointer to a newly initialized item.
  */
 func NewItem(name string,
-             category string,
-             Y int,
-             X int,
-             ch rune,
-             area *Area,
-             can_equip bool,
-             is_broken bool,
-             durability_current int,
-             durability_maximum int,
-             price_to_purchase int,
-             price_to_sell int,
-             weight int,
-             attack_increase int,
-             defence_increase int) *Item {
+	category string,
+	Y int,
+	X int,
+	ch rune,
+	area *Area,
+	canEquip bool,
+	isBroken bool,
+	durabilityCurrent int,
+	durabilityMaximum int,
+	priceToPurchase int,
+	priceToSell int,
+	weight int,
+	attackIncrease int,
+	defenceIncrease int) *Item {
 
-    // Return an address to a newly allocated Creature object.
-    return &Item{name,
-                 category,
-                 Y,
-                 X,
-                 ch,
-                 area,
-                 can_equip,
-                 is_broken,
-                 durability_current,
-                 durability_maximum,
-                 price_to_purchase,
-                 price_to_sell,
-                 weight,
-                 attack_increase,
-                 defence_increase}
+	// Return an address to a newly allocated Creature object.
+	return &Item{name,
+		category,
+		Y,
+		X,
+		ch,
+		area,
+		canEquip,
+		isBroken,
+		durabilityCurrent,
+		durabilityMaximum,
+		priceToPurchase,
+		priceToSell,
+		weight,
+		attackIncrease,
+		defenceIncrease}
 }
 
 //! Function to handle what occurs when the current durability of an item
@@ -125,60 +125,60 @@ func NewItem(name string,
  *
  * @return    none
  */
-func (itm *Item) adjust_durability(amount int) {
+func (itm *Item) adjustDurability(amount int) {
 
-    // If the durability was zero, then nothing to do.
-    if (amount == 0) {
-        DebugLog(&G, fmt.Sprintf("adjust_durability() --> was given amount " +
-                                 "of value zero, so nothing to be done..."))
-        return
-    }
+	// If the durability was zero, then nothing to do.
+	if amount == 0 {
+		DebugLog(&G, fmt.Sprintf("adjustDurability() --> was given amount "+
+			"of value zero, so nothing to be done..."))
+		return
+	}
 
-    // If debug, state how much the item durability currently is.
-    DebugLog(&G, fmt.Sprintf("adjust_durability() --> Item [%s] durability" +
-                             "before adjustment is: %d / %d",
-                             itm.name,
-                             itm.durability_current,
-                             itm.durability_maximum))
+	// If debug, state how much the item durability currently is.
+	DebugLog(&G, fmt.Sprintf("adjustDurability() --> Item [%s] durability"+
+		"before adjustment is: %d / %d",
+		itm.name,
+		itm.durabilityCurrent,
+		itm.durabilityMaximum))
 
-    // Adjust the current durability by the amount specified.
-    itm.durability_current += amount
+	// Adjust the current durability by the amount specified.
+	itm.durabilityCurrent += amount
 
-    // If debug, state how much the item durability has changed by.
-    DebugLog(&G, fmt.Sprintf("adjust_durability() --> item [%s] durability" +
-                             "after adjustment is: %d / %d",
-                             itm.name,
-                             itm.durability_current,
-                             itm.durability_maximum))
+	// If debug, state how much the item durability has changed by.
+	DebugLog(&G, fmt.Sprintf("adjustDurability() --> item [%s] durability"+
+		"after adjustment is: %d / %d",
+		itm.name,
+		itm.durabilityCurrent,
+		itm.durabilityMaximum))
 
-    // If the current item durability has reached beyond the max, go ahead
-    // and cap it to the maximum durability.
-    if itm.durability_current > itm.durability_maximum {
+	// If the current item durability has reached beyond the max, go ahead
+	// and cap it to the maximum durability.
+	if itm.durabilityCurrent > itm.durabilityMaximum {
 
-        // Cap the durability of the item.
-        itm.durability_current = itm.durability_maximum
+		// Cap the durability of the item.
+		itm.durabilityCurrent = itm.durabilityMaximum
 
-        // If debug mode, tell the developer what just happened.
-        DebugLog(&G, fmt.Sprintf("adjust_durability() --> item [%s] has " +
-                                 "exceeded max durability, and so has " +
-                                 "been capped"))
-    }
+		// If debug mode, tell the developer what just happened.
+		DebugLog(&G, fmt.Sprintf("adjustDurability() --> item [%s] has "+
+			"exceeded max durability, and so has "+
+			"been capped"))
+	}
 
-    // If the item is less than zero, then consider it to be broken, in
-    // which event t
-    if itm.durability_current < 1 {
+	// If the item is less than zero, then consider it to be broken, in
+	// which event t
+	if itm.durabilityCurrent < 1 {
 
-        // If debug mode, tell the developer this is going to break the item.
-        DebugLog(&G, fmt.Sprintf("adjust_durability() --> item [%s] is " +
-                                 "preparing to be broken", itm.name))
+		// If debug mode, tell the developer this is going to break the item.
+		DebugLog(&G, fmt.Sprintf("adjustDurability() --> item [%s] is "+
+			"preparing to be broken", itm.name))
 
-        // Set the relevant item properties via this function in order to
-        // render the item broken.
-        itm.event_broken()
+		// Set the relevant item properties via this function in order to
+		// render the item broken.
+		itm.eventBroken()
 
-        // Leave this routine, since the item broken event has been handled.
-        return
-    }
+		// Leave this routine, since the item broken event has been handled.
+		return
+	}
 }
 
 //! Function to handle what occurs when an item breaks.
@@ -187,15 +187,15 @@ func (itm *Item) adjust_durability(amount int) {
  *
  * @return    none
  */
-func (itm *Item) event_broken() {
+func (itm *Item) eventBroken() {
 
-    // Prevent the item from being equipped.
-    itm.can_equip = false
+	// Prevent the item from being equipped.
+	itm.canEquip = false
 
-    // Set the is_broken flag to true.
-    itm.is_broken = true
+	// Set the isBroken flag to true.
+	itm.isBroken = true
 
-    // If debug mode, tell the developer that the item is now broken.
-    DebugLog(&G, fmt.Sprintf("event_broken() --> item [%s] is now broken",
-                             itm.name))
+	// If debug mode, tell the developer that the item is now broken.
+	DebugLog(&G, fmt.Sprintf("eventBroken() --> item [%s] is now broken",
+		itm.name))
 }
