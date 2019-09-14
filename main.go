45f198a8fae6a0205565203ab4f4e097493d1a08
Robert Bisewski <contact@ibiscybernetics.com>
2019-09-13 22:16:12 -0500

    remove excess comments in main.go

--- main.go
+++ main.go
@@ -6,8 +6,6 @@
 
 package main
 
-// Import the types since the game needs to populate the creature type
-// for the purpose of spawning creatures later on.
 import (
 	"flag"
 	"fmt"
@@ -16,11 +14,7 @@ import (
 	"github.com/rbisewski/go_roguelike/types"
 )
 
-//
-// Global variable declaration.
-//
 var (
-	// Whether or not to print the current version of the program
 	printVersion = false
 	Version      = "0.0"
 
@@ -29,45 +23,36 @@ var (
 	PlayerName  string
 	PlayerClass *types.ClassTypeInfo
 
-	// DeveloperMode ... global debug mode
+	// global debug mode
 	DeveloperMode = false
 
-	// MenuErrorMsg ... global error message via menu
+	// global error message via menu
 	MenuErrorMsg string
 
-	// GlobalCreatureTypeInfoMap ... Global variable to hold all of the creature
-	// types.
+	// map for all of the creature types
 	GlobalCreatureTypeInfoMap = make(map[string]types.CreatureTypeInfo)
 
-	// GlobalCreatureTypeInfoMapIsPopulated ... Global variable to check if the
-	// creature types has already been populated.
+	// check if the creature types has already been populated.
 	GlobalCreatureTypeInfoMapIsPopulated = false
 
-	// GlobalItemTypeInfoMap ... global variable to hold all of the item types.
+	// map for all of the item types.
 	GlobalItemTypeInfoMap = make(map[string]types.ItemTypeInfo)
 
-	// GlobalItemTypeInfoMapIsPopulated ... Global variable to check if the item
-	// types has already been populated.
+	// check if the item types has already been populated.
 	GlobalItemTypeInfoMapIsPopulated = false
 
-	// GlobalClassTypeInfoMap ... global variable to hold all of the class types.
+	// map of all of the class types.
 	GlobalClassTypeInfoMap = make(map[string]types.ClassTypeInfo)
 
-	// GlobalClassTypeInfoMapIsPopulated ... Global variable to check if the
-	// class types has already been populated.
+	// check if the class types has already been populated.
 	GlobalClassTypeInfoMapIsPopulated = false
 )
 
 func init() {
-
-	// Version mode flag
 	flag.BoolVar(&printVersion, "version", false,
 		"Print the current version of this program and exit.")
 }
 
-//
-// Main
-//
 func main() {
 
 	flag.Parse()
